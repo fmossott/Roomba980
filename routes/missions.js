a6cff5598d1e4a4b973781d91250e494b8794575
Franco Mossotto <franco.mossotto@gmail.com>
2017-10-26 22:49:11 +0200

    Server: Autolog missions, 'roomba' base path, missions apis Map: load of current mission at startup, autoretry on failure

---
+++ routes/missions.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+var missionsPath = path.join(__dirname, '../missions');
+
+router.get('/', function (req, res) {
+  var fs = require('fs');
+  var files = fs.readdir(missionsPath, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(files);
+    }
+  });
+});
+
+router.use(express.static(missionsPath));
+
+module.exports = router;
