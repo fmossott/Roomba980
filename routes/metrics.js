703cfe3f7dac179cb0d3960573212b403dc36738
Franco Mossotto <fmossott@users.noreply.github.com>
2021-02-04 10:43:16 +0100

    Added metric to track when battery is actually charging

--- routes/metrics.js
+++ routes/metrics.js
@@ -8,10 +8,7 @@ const batPctGauge = new client.Gauge({ name: prefix + 'battery', help: 'Roomba b
 const chargingGauge = new client.Gauge({ name: prefix + 'charging', help: 'Roomba battery is charging: 0/1' });
 const activeChargingGauge = new client.Gauge({ name: prefix + 'active_charging', help: 'Roomba battery is activily charging (charging and the battery is less than 100%): 0/1' });
 const hasMissionGauge = new client.Gauge({ name: prefix + 'has_mission', help: 'Roomba has a mission: 0/1' });
-const lastMissionDurationGauge = new client.Gauge({ name: prefix + 'last_mission_duration', help: 'Duration of the last completed Roomba mission in seconds' });
 
-let prevHasMission = -1;
-let missionStart;
 let lastBatPct = 0;
 let lastCharging = 0;
 
@@ -42,22 +39,6 @@ function onUpdate (msg) {
     const hasMission = (cycle !== 'none' && cycle !== 'dock' ? 1 : 0);
     hasMissionGauge.set(hasMission);
     console.log('metrics.js - set hasMissionGauge: ' + hasMission);
-
-    if (prevHasMission !== -1) {
-      if (!prevHasMission && hasMission) {
-        // was stop and now has started
-        missionStart = Date.now();
-        console.log('metrics.js - Mission started at ' + new Date(missionStart));
-      } else if (prevHasMission && !hasMission) {
-        // was running and now has stopped
-        const missionEnd = Date.now();
-        console.log('metrics.js - Mission stopped at ' + new Date(missionEnd));
-        const duration = Math.floor((missionEnd - missionStart) / 1000);
-        console.log('metrics.js - Mission duration ' + duration);
-        lastMissionDurationGauge.set(duration);
-      }
-    }
-    prevHasMission = hasMission;
 
     // Check charging
     lastCharging = (phase === 'charge' ? 1 : 0);
